a05a2bc3e4f17bf668eda2dd386f692cfa510724
CHEVALEYRE Antoine <antoine.chevaleyre6@etu.univ-lorraine.fr>
2022-03-08 15:01:02 +0100

    ajout index

---
+++ PHP/index.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+namespace Mygamepedia;
+
+session_start();
+require 'vendor/autoload.php';
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+use \Illuminate\Database\Capsule\Manager as DB;
+use \Slim\Container;
+use \Slim\App;
+
+$configuration = [
+    'settings' => [
+    'displayErrorDetails' => true,
+    'dbconf' => '/conf/db.conf.ini' ]
+    ];
+    $c = new Container($configuration);
+    $app = new App($c);
+
+$db = new DB();
+$db->addConnection(parse_ini_file('src/conf/conf.ini'));
+$db->setAsGlobal();
+$db->bootEloquent();
+
