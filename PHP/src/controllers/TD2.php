5d2a8ba8c597f923d04e76805da2b787520dd86a
Littleredshift <jarosz1u@etu.univ-lorraine.fr>
2022-03-15 16:23:59 +0100

    début TD2 q1

---
+++ PHP/src/controllers/TD2.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Mygamepedia\controllers;
+
+use Mygamepedia\models\Character;
+use Mygamepedia\models\Game;
+use Mygamepedia\models\Company;
+use Mygamepedia\models\Platform;
+
+class TD2{
+
+
+    public static function q1(){
+        echo "<br><br> \n Personnages du Jeu 12342 :\n";
+        foreach (Character::with("game2character")->were("game_id","=",12342)->get() as $chara) {
+            foreach ($chara->Game as $game){
+                echo "<br>";
+                echo "  " . $chara->name ." : " . $chara->deck . "\n";
+            }
+        }
+        echo "\n";
+    }
+
+
+
+}
