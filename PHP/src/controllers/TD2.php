ec75ad00e1544e2643240c31474a6b6d22826910
CHEVALEYRE Antoine <antoine.chevaleyre6@etu.univ-lorraine.fr>
2022-03-28 08:56:15 +0200

    j

--- PHP/src/controllers/TD2.php
+++ PHP/src/controllers/TD2.php
@@ -21,6 +21,21 @@ class TD2{
     }
     public static function q2(){
         echo "<br><br> \n Personnages des jeux commençant par Mario :\n";
-        
+
     }
+
+    public static function q3()
+        {
+            echo "<br><br> \n jeux développés par une compagnie dont le nom contient 'Sony':\n";
+            $companies = Company::where("name", "LIKE", "%Sony%")->get()->toArray();
+            foreach ($companies as $c) {
+                echo "    - " . $c['name'] ."\n";
+                $games = Company::where("id", "=", $c['id'])->first()->game_developers->toArray();
+                foreach ($games as $g) {
+                    echo "        - " . $g['name'] ."\n";
+                }
+            }
+        }
+
+
 }
