2709e8b8406288d1cae2027ce2ca66e9c8b3a0fc
Littleredshift <jarosz1u@etu.univ-lorraine.fr>
2022-03-29 14:52:59 +0200

    f p1 td4

--- PHP/src/controllers/TD4.php
+++ PHP/src/controllers/TD4.php
@@ -9,10 +9,18 @@ class TD4
 {
 
     public static function p1(){
+        //Supp data
         $uti = Utilisateur::where("email","like","test@mail")->delete();
         $uti = Utilisateur::where("email","like","test2@mail")->delete();
         $uti = Utilisateur::where("email","like","test3@mail")->delete();
+        foreach(Commentaire::where("utilisateur_email","like","test@mail")->get() as $coms){
+               $coms->delete();
+        }
+        foreach(Commentaire::where("utilisateur_email","like","test2@mail")->get() as $coms){
+            $coms->delete();
+        }
 
+        //Create util
         $u1 = Utilisateur::create([
             "email"=>"test@mail", "nom"=>"Didier", "prenom"=>"Jean", "adresse"=>"6 rue des dames",
             "num_tel"=>06666666, "date_naiss"=>"2002-12-20"
@@ -22,12 +30,36 @@ class TD4
             "num_tel"=>06666666, "date_naiss"=>"2002-03-01"
         ])->save();
 
-        $t =time();
+        //Comm 1st util
+        $t = date('Y-m-d H:i:s');
         echo $t;
-        /*
+
         $com1 = Commentaire::create([
-            "titre"=>"test","contenu"=>"test","creat_at","update_at","utilisateur_email"=>"test@mail","game_id"=>12342
+            "titre"=>"test","contenu"=>"test","create_at"=>$t,"update_at"=>$t,"utilisateur_email"=>"test@mail","game_id"=>12342
         ])->save();
-        */
+        $t = date('Y-m-d H:i:s');
+        $com1 = Commentaire::create([
+            "titre"=>"test2","contenu"=>"test32","create_at"=>$t,"update_at"=>$t,"utilisateur_email"=>"test@mail","game_id"=>12342
+        ])->save();
+        $t = date('Y-m-d H:i:s');
+        $com1 = Commentaire::create([
+            "titre"=>"test3","contenu"=>"te764","create_at"=>$t,"update_at"=>$t,"utilisateur_email"=>"test@mail","game_id"=>12342
+        ])->save();
+
+        //Comm 2em util
+        $t = date('Y-m-d H:i:s');
+        $com1 = Commentaire::create([
+            "titre"=>"walla la diguerie","contenu"=>"test","create_at"=>$t,"update_at"=>$t,"utilisateur_email"=>"test2@mail","game_id"=>12342
+        ])->save();
+        $t = date('Y-m-d H:i:s');
+        $com1 = Commentaire::create([
+            "titre"=>"test 2267","contenu"=>"je teste","create_at"=>$t,"update_at"=>$t,"utilisateur_email"=>"test2@mail","game_id"=>12342
+        ])->save();
+        $t = date('Y-m-d H:i:s');
+        $com1 = Commentaire::create([
+            "titre"=>"...","contenu"=>"te764","create_at"=>$t,"update_at"=>$t,"utilisateur_email"=>"test2@mail","game_id"=>12342
+        ])->save();
+
+
     }
 }
--- PHP/src/models/Commentaire.php
+++ PHP/src/models/Commentaire.php
@@ -5,7 +5,7 @@ class Commentaire extends \Illuminate\Database\Eloquent\Model
     protected $table = 'commentaire';
     protected $primaryKey = 'id';
     public $timestamps = false;
-    public $fillable = ["titre","contenu","creat_at","update_at","utilisateur_email","game_id"];
+    public $fillable = ["titre","contenu","create_at","update_at","utilisateur_email","game_id"];
 
     public function utilisateur() {
         return $this->belongsTo('Mygamepedia\models\Utilisateur','utilisateur_email');
